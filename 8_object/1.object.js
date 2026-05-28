b4d3b7eaa53335f00d750368378e2e7e75ee7d39
tirthsalot <salottirth09@gmail.com>
2026-05-28 12:55:09 +0530

    Add files via upload

---
+++ 8_object/1.object.js
@@ -0,0 +1,40 @@
+
+
+const personDetails = {
+  name: "tirth",
+  age: 19,
+};
+
+console.log(typeof personDetails);
+
+console.log("personDetails", personDetails);
+
+
+const student = new Object ();
+
+console.log(typeof student);
+
+student.name = "rutik";
+
+student.age = 19;
+
+student.address = "state of dakana";
+
+student.course = "career x ai";
+
+console.log(student)
+
+
+const employee = new Object ();
+
+console.log(typeof employee);
+
+employee.name = "alice";
+
+employee.age = 28;
+
+employee.workduration = "8hour";
+
+employee.department = "Finance";
+
+console.log(employee)
