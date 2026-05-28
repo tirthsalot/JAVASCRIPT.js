b359790bbe71334c07660ced8058d2b7fd81bf4b
tirthsalot <salottirth09@gmail.com>
2026-05-28 12:54:45 +0530

    Add files via upload

---
+++ 7_string/3.string_method.js
@@ -0,0 +1,84 @@
+
+let text1 = "Good evening";
+
+console.log("split string into array with space added ", text1.split(" "));
+
+console.log("split string into array without space", text1.split(""));
+
+console.log("split string with o", text1.split("e"));
+
+let text2 = "welcome to the javascript";
+
+console.log("split string into array without space", text2.split(""));
+
+console.log("split string into array with space added ", text2.split(" "));
+
+console.log("split string with t", text2.split("t"));
+
+console.log("startswith method", text2.startsWith("welcome"));
+
+let text3 = "Good morning";
+
+console.log("split string into array with space added ",text3.split(" "));
+
+console.log("split string into array without space added ", text3.split(""));
+
+console.log("split starting with g", text3.split("n"));
+
+console.log("startswith method", text3.startsWith("morning"));
+
+
+let text4 = "we can learn about javascript language";
+
+console.log("substring : ",text4.substring(5));
+
+console.log("substring : ",text4.substring(1,8));
+
+console.log("substring : ",text4.substring(12,2));
+
+let text5 = "red and white skill education bhavnagar";
+
+console.log("substring example", text5.substring(2));
+
+console.log("substring op =>", text5.substring(1, 4));
+
+console.log("substring op =>", text5.substring(-1));
+
+let text6 = "javascript";
+console.log("uppercase : ",text5.toLocaleUpperCase());
+
+let text7 = "JAVASCRIPT";
+console.log("lowercase : ",text5.toLocaleLowerCase());
+
+let text8 = "  ===Fullstack developer===   ";
+
+console.log(text8.trim());
+
+console.log(text8.trimStart());
+
+console.log(text8.trimEnd());
+
+console.log(text8.trimEnd());
+
+console.log(text8.trimStart());
+
+console.log(text8.trim());
+
+console.log(text8.trimEnd());
+
+console.log(text8.trimEnd());
+
+console.log(text8.trimStart());
+
+let text9 = "TIRTH";
+
+console.log(text9.toLocaleLowerCase());
+
+let text10 = "WATCH THE MOVIE";
+
+console.log(text10.toLocaleLowerCase());
+
+let text11 = "PLAY THE CRICKET";
+
+console.log(text11.toLocaleLowerCase());
+
