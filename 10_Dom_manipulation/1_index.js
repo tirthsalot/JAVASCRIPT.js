7854c66901b15bd7c149e30bdd3bc662e3758dbb
tirthsalot <salottirth09@gmail.com>
2026-06-11 13:25:42 +0530

    Add files via upload

---
+++ 10_Dom_manipulation/1_index.js
@@ -0,0 +1,11 @@
+// id selector
+
+document.getElementById("heading").innerText = "this is red and white skill education";
+
+// class selector
+
+let workflow = document.getElementsByClassName("work")[0].innerHTML="work in js";
+
+// tag selector
+
+let tagselector = document.getElementsByTagName("h6")[0].innerHTML="Heading Level 6 element.";
