08f4a47e7cb9a95a633d15fda000ec47ff99f3c9
tirthsalot <salottirth09@gmail.com>
2026-07-14 13:58:37 +0530

    Update script.js

--- 18_E-commerce/script.js
+++ 18_E-commerce/script.js
@@ -1,46 +1,188 @@
+let products = [];
+
+let cartItems = JSON.parse(localStorage.getItem("cartData")) || [];
+
 async function showProduct() {
-  const productList = document.getElementById("product-list");
 
-  try {
-    const response = await fetch(
-      "https://kolzsticks.github.io/Free-Ecommerce-Products-Api/main/products.json",
-    );
+    const productList = document.getElementById("product-list");
 
-    const productData = await response.json();
+    try {
 
-    productList.innerHTML = "";
+        const response = await fetch(
+            "https://kolzsticks.github.io/Free-Ecommerce-Products-Api/main/products.json"
+        );
 
-    productData.forEach((p) => {
-      productList.innerHTML += `
-        <div class="col-md-4 mt-3">
-          <div class="card product-card shadow rounded-4 h-100">
-            <img src="${p.image}" class="card-img-top img-fluid rounded-4" alt="${p.name}">
-            
-            <div class="card-body text-center">
-              <h5 class="card-title">${p.name}</h5>
-              <h4 class="card-text">₹${p.priceCents}</h4>
+        products = await response.json();
 
-              <button class="btn btn-outline-primary" onclick="addToCart(${p.id})">
-                Add to Cart
-              </button>
+        productList.innerHTML = "";
+
+        products.forEach((p) => {
+
+            productList.innerHTML += `
+
+            <div class="col-md-4 mb-4">
+
+                <div class="card h-100 shadow">
+
+                    <img src="${p.image}" class="card-img-top" height="250">
+
+                    <div class="card-body text-center">
+
+                        <h5>${p.name}</h5>
+
+                        <h4>₹${(p.priceCents / 100).toFixed(2)}</h4>
+
+                        <button class="btn btn-primary"
+                        onclick="addToCart('${p.id}')">
+
+                        Add To Cart
+
+                        </button>
+
+                    </div>
+
+                </div>
+
             </div>
-          </div>
-        </div>
-      `;
-    });
 
-    console.log(productData);
-  } catch (error) {
-    console.error("Error fetching products:", error);
-  }
+            `;
+        });
+
+    }
+
+    catch (error) {
+
+        console.log(error);
+
+    }
+
 }
 
 showProduct();
 
-localStorage.setItem("cart", JSON.stringify(cart));
+function addToCart(id) {
 
-let data = JSON.parse(localStorage.getItem("cart"));
+  try {
 
-localStorage.removeItem("cart");
+    let product = cartItems.find(item => item.id == id);
 
-localStorage.clear();
+    console.log("Product Added to Cart");
+
+    if (product) {
+
+        product.qty++;
+
+    }
+
+    else {
+
+        product = products.find(item => item.id == id);
+
+        cartItems.push({...product, qty: 1})
+
+    }
+
+    alert("Product Added Successfully");
+
+    updateLatestData();
+
+    console.log("Product Added to Cart:", cartItems);
+
+  }
+catch (error) {
+
+    console.log("Error")
+  };
+
+  
+const update = ()=> {
+
+  localStorage.setItem("cartData", JSON.stringify(cartItems));
+
+}
+
+
+}
+
+function showModal() {
+
+    let modal = new bootstrap.Modal(document.getElementById("cartList"));
+
+    modal.show();
+
+    updateLatestData();
+
+}
+
+function updateLatestData() {
+
+  try {
+
+    let table = document.getElementById("cartTable");
+
+
+    table.innerHTML = "";
+
+
+
+    cartItems.forEach((p) => {
+
+
+        table.innerHTML += `
+
+        <tr>
+
+      <td>${p.id}</td>
+      <td><img src="${p.image}" class="img-fluid" height="40px" width="35px"/></td>
+    <td>${p.name}</td>
+        <td>${p.priceCents}</td>
+              <td>${p.qty}</td>
+
+        <div class= "d-flex gap-2 align-item-center justify-contact-center">
+        
+        
+        
+        </div>      
+
+      </tr>
+    
+      `
+    })
+
+    }catch(error){
+
+      console.log(error)
+    }
+
+
+}
+
+// function removeItem(index) {
+
+//     cartItems.splice(index, 1);
+
+//     localStorage.setItem("cartData", JSON.stringify(cartItems));
+
+//     updateLatestData();
+
+// }
+
+// function checkOut() {
+
+//     if (cartItems.length == 0) {
+
+//         alert("Cart is Empty");
+
+//         return;
+
+//     }
+
+//     alert("Order Placed Successfully");
+
+//     cartItems = [];
+
+//     localStorage.setItem("cartData", JSON.stringify(cartItems));
+
+//     updateLatestData();
+
+// }
